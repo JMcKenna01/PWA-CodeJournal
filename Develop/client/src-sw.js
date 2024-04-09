f523d842ce75f47795b4d6e30b335b2a25844879
jmckenna01 <jmckenna2026@gmail.com>
2024-04-09 11:10:14 -0400

    implemented asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -5,26 +5,49 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache application shell
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Strategy for caching pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
-      statuses: [0, 200],
+      statuses: [0, 200], // Cache successful responses
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
     }),
   ],
 });
 
+// Warm up the page cache
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Register route for navigating requests
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset caching
+registerRoute(
+  // Match files based on their request destination
+  ({request}) => request.destination === 'style' ||
+                 request.destination === 'script' ||
+                 request.destination === 'worker',
+  // Use a CacheFirst strategy for caching assets
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200], // Only cache successful responses
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60, // Limit the number of entries in the cache
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache assets for 30 days
+        purgeOnQuotaError: true, // Clean up cache if quota is exceeded
+      }),
+    ],
+  })
+);
