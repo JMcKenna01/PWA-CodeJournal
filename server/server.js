914139596c49a30dd513428ddd417cfbad66a78e
jmckenna01 <jmckenna2026@gmail.com>
2024-04-09 13:41:08 -0400

    added to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path'); // Import the path module
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Define the directory path to serve static files from
+const staticDirPath = path.join(__dirname, '../client/dist');
+
+// Serve static files from the defined directory
+app.use(express.static(staticDirPath));
+
+// Set up body parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Import and set up your HTML routes
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
